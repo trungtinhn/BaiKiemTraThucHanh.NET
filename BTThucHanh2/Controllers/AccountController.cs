82709bb5e07fe38809efdda6cf7ccb413c153ece
trungtinhn <trungtinh1620@gmail.com>
2024-10-28 15:09:52 +0700

    Add project files.

---
+++ BTThucHanh2/Controllers/AccountController.cs
@@ -0,0 +1,71 @@
+﻿using BTThucHanh2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTThucHanh2.Controllers
+{
+    public class AccountController : Controller
+    {
+        QlbanVaLiContext db = new QlbanVaLiContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            if (HttpContext.Session.GetString("UserName") == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        [HttpPost]
+        public IActionResult Login(TUser user)
+        {
+            if (HttpContext.Session.GetString("UserName") == null)
+            {
+                var u = db.TUsers.Where(x => x.Username.Equals(user.Username) && x.Password.Equals(user.Password)).FirstOrDefault();
+                if (u != null)
+                {
+                    HttpContext.Session.SetString("UserName", u.Username.ToString());
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(TUser user, string confirmPassword)
+        {
+            // Kiểm tra nếu tên đăng nhập đã tồn tại
+            if (db.TUsers.Any(x => x.Username == user.Username))
+            {
+                ViewBag.Error = "Tên đăng nhập đã tồn tại";
+                return View();
+            }
+
+            // Kiểm tra nếu mật khẩu và xác nhận mật khẩu khớp nhau
+            if (user.Password != confirmPassword)
+            {
+                ViewBag.Error = "Mật khẩu và xác nhận mật khẩu không khớp";
+                return View();
+            }
+
+            // Nếu hợp lệ, thêm người dùng mới vào cơ sở dữ liệu
+            db.TUsers.Add(user);
+            db.SaveChanges();
+
+            // Chuyển hướng người dùng sang trang đăng nhập sau khi đăng ký thành công
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ BTThucHanh2/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using BTThucHanh2.Models;
+using BTThucHanh2.Models.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using X.PagedList;
+
+namespace BTThucHanh2.Controllers
+{
+    public class HomeController : Controller
+    {
+        QlbanVaLiContext db = new QlbanVaLiContext();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Authentication]
+
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 8;
+            int pageNumber = page==null||page < 0 ? 1 : page.Value;
+            var lstsanpham = db.TDanhMucSps.AsNoTracking().OrderBy(x=>x.TenSp);
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp> (lstsanpham, pageNumber, pageSize );
+            return View(lst);
+        }
+        public IActionResult ChiTietSanPham(string maSp)
+        {
+            var sanPham = db.TDanhMucSps.SingleOrDefault(x=> x.MaSp==maSp);
+            var anhSanPham = db.TAnhSps.Where(x=> x.MaSp==maSp).ToList();
+            ViewBag.anhSanPham = anhSanPham;
+            return View(sanPham);
+        }
+         
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
